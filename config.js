d9aa0958b56e3d9b9da6aaf49f9b081ba9d99ad3
Alfred Rodriguez <47036123+alfredrodriguez98@users.noreply.github.com>
2022-10-17 08:24:56 -0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const MINE_RATE = 1000; //1s = 1000ms
+const INITIAL_DIFFICULTY = 2;
+const GENESIS_DATA = {
+  timestamp: 1,
+  prevHash: "0x000",
+  hash: "0x123",
+  data: [],
+  nonce: 0,
+  difficulty: INITIAL_DIFFICULTY,
+};
+
+module.exports = { GENESIS_DATA, MINE_RATE };
